eaeccee0603907848bf5a60b567e58d553d4e4b4
Hien Nguyen <hiennguyen92@gmail.com>
2021-10-07 04:57:21 +0700

    update ios version

---
+++ ios/Classes/Call.swift
@@ -0,0 +1,8 @@
+//
+//  Call.swift
+//  flutter_callkit_incoming
+//
+//  Created by Hien Nguyen on 07/10/2021.
+//
+
+import Foundation
---
+++ ios/Classes/CallManager.swift
@@ -0,0 +1,8 @@
+//
+//  CallManager.swift
+//  flutter_callkit_incoming
+//
+//  Created by Hien Nguyen on 07/10/2021.
+//
+
+import Foundation
