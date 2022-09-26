86dd53cf94e6aaaa92d0f0315de2374fd37c0d98
icodelifee <mithunsmenon@gmail.com>
2022-09-26 15:14:18 +0530

    feat: update example app to use new dart models

--- example/lib/calling_page.dart
+++ example/lib/calling_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_callkit_incoming/entities/entities.dart';
 import 'package:flutter_callkit_incoming/flutter_callkit_incoming.dart';
 import 'package:flutter_callkit_incoming_example/navigation_service.dart';
 import 'package:http/http.dart';
@@ -11,52 +14,53 @@ class CallingPage extends StatefulWidget {
 }
 
 class CallingPageState extends State<CallingPage> {
-  late dynamic calling;
+  late CallKitParams? calling;
 
   @override
   Widget build(BuildContext context) {
-    calling = ModalRoute.of(context)!.settings.arguments;
-    print(calling);
+    final params = jsonDecode(jsonEncode(ModalRoute.of(context)!.settings.arguments as Map<dynamic, dynamic>));
+    calling = CallKitParams.fromJson(params);
+    debugPrint(calling?.toJson().toString());
 
     return Scaffold(
-        body: Container(
-            height: MediaQuery.of(context).size.height,
-            width: double.infinity,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('Calling...'),
-                  TextButton(
-                    style: ButtonStyle(
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.blue),
-                    ),
-                    onPressed: () async {
-                      FlutterCallkitIncoming.endCall(calling);
-                      calling = null;
-                      NavigationService.instance.goBack();
-                      await requestHttp('END_CALL');
-                    },
-                    child: Text('End Call'),
-                  )
-                ],
-              ),
-            )));
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Calling...'),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                onPressed: () async {
+                  if (calling != null) {
+                    FlutterCallkitIncoming.endCall(calling!.id!);
+                    calling = null;
+                  }
+                  NavigationService.instance.goBack();
+                  await requestHttp('END_CALL');
+                },
+                child: Text('End Call'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   //check with https://webhook.site/#!/2748bc41-8599-4093-b8ad-93fd328f1cd2
   Future<void> requestHttp(content) async {
-    get(Uri.parse(
-        'https://webhook.site/2748bc41-8599-4093-b8ad-93fd328f1cd2?data=$content'));
+    get(Uri.parse('https://webhook.site/2748bc41-8599-4093-b8ad-93fd328f1cd2?data=$content'));
   }
 
   @override
   void dispose() {
     super.dispose();
-    if (calling != null) {
-      FlutterCallkitIncoming.endCall(calling);
-    }
+    if (calling != null) FlutterCallkitIncoming.endCall(calling!.id!);
   }
 }
