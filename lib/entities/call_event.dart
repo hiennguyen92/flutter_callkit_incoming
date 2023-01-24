936e63f73d0aa8119fa1efef46c606851c0b5efc
AAkira <developer.a.akira@gmail.com>
2023-01-24 11:41:46 +0900

    Update CallEvent.

--- lib/entities/call_event.dart
+++ lib/entities/call_event.dart
@@ -2,16 +2,180 @@ import 'package:flutter_callkit_incoming/entities/call_kit.dart';
 
 /// Object CallEvent.
 class CallEvent {
-  CallEvent({
-    required this.event,
-    this.callKit,
-  });
+  CallEvent._();
 
-  final Event event;
-  final CallKit? callKit;
+  factory CallEvent.incoming(CallKit callkit) = CallActionIncoming;
+
+  factory CallEvent.start(CallKit callkit) = CallActionStart;
+
+  factory CallEvent.accept(CallKit callkit) = CallActionAccept;
+
+  factory CallEvent.decline(CallKit callkit) = CallActionDecline;
+
+  factory CallEvent.ended(CallKit callkit) = CallActionEnded;
+
+  factory CallEvent.timeout(CallKit callkit) = CallActionTimeout;
+
+  factory CallEvent.callback(CallKit callkit) = CallActionCallback;
+
+  factory CallEvent.updateDevicePushToken(String deviceToken) =
+      CallActionUpdateDevicePushToken;
+
+  factory CallEvent.toggleHold(String id, bool isOnHold) = CallActionToggleHold;
+
+  factory CallEvent.toggleMute(String id, bool isMuted) = CallActionToggleMute;
+
+  factory CallEvent.toggleDMTF(String id, String digits, DTMFActionType type) =
+      CallActionToggleDMTF;
+
+  factory CallEvent.toggleGroup(String id, String callUUIDToGroupWith) =
+      CallActionToggleGroup;
+
+  factory CallEvent.toggleAudioSession(bool isActivate) = CallActionToggleAudioSession;
+}
+
+class CallActionIncoming extends CallEvent {
+  CallActionIncoming(this.callKit) : super._();
+
+  final CallKit callKit;
 
   @override
-  String toString() => 'CallEvent(event: $event, callkit: $callKit)';
+  String toString() => 'CallEvent.incoming(callkit: $callKit)';
+}
+
+class CallActionStart extends CallEvent {
+  CallActionStart(this.callKit) : super._();
+
+  final CallKit callKit;
+
+  @override
+  String toString() => 'CallEvent.start(callkit: $callKit)';
+}
+
+class CallActionAccept extends CallEvent {
+  CallActionAccept(this.callKit) : super._();
+
+  final CallKit callKit;
+
+  @override
+  String toString() => 'CallEvent.accept(callkit: $callKit)';
+}
+
+class CallActionDecline extends CallEvent {
+  CallActionDecline(this.callKit) : super._();
+
+  final CallKit callKit;
+
+  @override
+  String toString() => 'CallEvent.decline(callkit: $callKit)';
+}
+
+class CallActionEnded extends CallEvent {
+  CallActionEnded(this.callKit) : super._();
+
+  final CallKit callKit;
+
+  @override
+  String toString() => 'CallEvent.ended(callkit: $callKit)';
+}
+
+class CallActionTimeout extends CallEvent {
+  CallActionTimeout(this.callKit) : super._();
+
+  final CallKit callKit;
+
+  @override
+  String toString() => 'CallEvent.timeout(callkit: $callKit)';
+}
+
+class CallActionCallback extends CallEvent {
+  CallActionCallback(this.callKit) : super._();
+
+  final CallKit callKit;
+
+  @override
+  String toString() => 'CallEvent.callback(callkit: $callKit)';
+}
+
+class CallActionUpdateDevicePushToken extends CallEvent {
+  CallActionUpdateDevicePushToken(this.deviceToken) : super._();
+
+  final String deviceToken;
+
+  @override
+  String toString() =>
+      'CallEvent.updateDevicePushToken(deviceToken: $deviceToken)';
+}
+
+class CallActionToggleHold extends CallEvent {
+  CallActionToggleHold(
+    this.id,
+    this.isOnHold,
+  ) : super._();
+
+  final String id;
+  final bool isOnHold;
+
+  @override
+  String toString() => 'CallEvent.toggleHold(id: $id, isOnHold: $isOnHold)';
+}
+
+class CallActionToggleMute extends CallEvent {
+  CallActionToggleMute(
+    this.id,
+    this.isMuted,
+  ) : super._();
+
+  final String id;
+  final bool isMuted;
+
+  @override
+  String toString() => 'CallEvent.toggleMute(id: $id, isMuted: $isMuted)';
+}
+
+class CallActionToggleDMTF extends CallEvent {
+  CallActionToggleDMTF(
+    this.id,
+    this.digits,
+    this.type,
+  ) : super._();
+
+  final String id;
+  final String digits;
+  final DTMFActionType type;
+
+  @override
+  String toString() =>
+      'CallEvent.toggleDMTF(id: $id, digits: $digits, type: $type)';
+}
+
+class CallActionToggleGroup extends CallEvent {
+  CallActionToggleGroup(
+    this.id,
+    this.callUUIDToGroupWith,
+  ) : super._();
+
+  final String id;
+  final String callUUIDToGroupWith;
+
+  @override
+  String toString() =>
+      'CallEvent.toggleGroup(id: $id, callUUIDToGroupWith: $callUUIDToGroupWith)';
+}
+
+class CallActionToggleAudioSession extends CallEvent {
+  CallActionToggleAudioSession(this.isActivate) : super._();
+
+  final bool isActivate;
+
+  @override
+  String toString() => 'CallEvent.toggleAudioSession(isActivate: $isActivate)';
+}
+
+enum DTMFActionType {
+  singleTone,
+  softPause,
+  hardPause,
 }
 
 enum Event {
