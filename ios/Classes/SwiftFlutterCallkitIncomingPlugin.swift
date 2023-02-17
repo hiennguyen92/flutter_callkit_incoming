2917d21f10c2f860d5392daa11ce8c2bbd96f5d2
Magda Budzińska <magda.budzinska@goodsoft.pl>
2023-02-17 08:22:03 +0100

    Fix Decline&Accept action

--- ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
+++ ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
@@ -410,7 +410,7 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
                 action.fulfill()
             }
         }else {
-            sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_ENDED, self.data?.toJSON())
+            sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_ENDED, call.data.toJSON())
             action.fulfill()
         }
     }
@@ -496,10 +496,7 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
             self.outgoingCall = nil
         }
         self.answerCall?.endCall()
-        if(self.answerCall != nil){
-            self.answerCall = nil
-        }
-        self.callManager?.removeAllCalls()
+      
         self.sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_TOGGLE_AUDIO_SESSION, [ "isActivate": false ])
     }
     
