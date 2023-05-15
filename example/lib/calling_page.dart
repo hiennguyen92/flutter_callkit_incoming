4174dfaf3f167b51858744bf8ccba7f765d9d388
ryojiro <ryojirok@gmail.com>
2023-05-15 20:15:32 +0900

    example: Fix prefer_interpolation_to_compose_strings. Ref https://dart-lang.github.io/linter/lints/prefer_interpolation_to_compose_strings.html.

--- example/lib/calling_page.dart
+++ example/lib/calling_page.dart
@@ -39,12 +39,9 @@ class CallingPageState extends State<CallingPage> {
     h = value ~/ 3600;
     m = ((value - h * 3600)) ~/ 60;
     s = value - (h * 3600) - (m * 60);
-    String hourLeft =
-        h.toString().length < 2 ? "0" + h.toString() : h.toString();
-    String minuteLeft =
-        m.toString().length < 2 ? "0" + m.toString() : m.toString();
-    String secondsLeft =
-        s.toString().length < 2 ? "0" + s.toString() : s.toString();
+    String hourLeft = h.toString().length < 2 ? '0$h' : h.toString();
+    String minuteLeft = m.toString().length < 2 ? '0$m' : m.toString();
+    String secondsLeft = s.toString().length < 2 ? '0$s' : s.toString();
     String result = "$hourLeft:$minuteLeft:$secondsLeft";
     return result;
   }
