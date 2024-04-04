bfc863816bddb411bdbff08092bac08d95a13878
andre.ferraz <andre.ferraz@BRSPPM-CN96WF7Y4Y.local>
2024-04-04 11:23:47 -0300

    change intent

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/AppUtils.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/AppUtils.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 
 object AppUtils {
     fun getAppIntent(context: Context, action: String? = null, data: Bundle? = null): Intent? {
-        val intent = context.packageManager.getLaunchIntentForPackage(context.packageName)?.cloneFilter()
+        val intent = "com.noknox.morador.flutter.FlutterCallActivity"
         intent?.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_REORDER_TO_FRONT or Intent.FLAG_ACTIVITY_CLEAR_TOP)
         intent?.putExtra(FlutterCallkitIncomingPlugin.EXTRA_CALLKIT_CALL_DATA, data)
         intent?.action = action
