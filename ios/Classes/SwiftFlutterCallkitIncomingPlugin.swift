11ef1923e033a88cfb5a55872c34c9809b995352
Alex Babich <Alexander.Babich@instantconnectnow.com>
2024-08-28 12:13:50 +0300

    - Ignoring audio session configuration

--- ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
+++ ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
@@ -434,6 +434,9 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
     }
     
     func configurAudioSession(){
+        print("Ignoring audio session configuration")
+        return
+
         if data?.configureAudioSession != false {
             let session = AVAudioSession.sharedInstance()
             do{
