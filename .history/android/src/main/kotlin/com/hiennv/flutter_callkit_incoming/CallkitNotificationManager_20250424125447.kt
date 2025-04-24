e2e903f247a065052339af427f00dd658bdc36ee
Sandesh Ghimire <88843968+kashyapsandesh@users.noreply.github.com>
2025-04-24 14:43:07 +0545

    removing icon done

--- .history/android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager_20250424125447.kt
+++ .history/android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager_20250424125447.kt
@@ -200,14 +200,14 @@ class CallkitNotificationManager(private val context: Context) {
                 notificationBuilder.setContentTitle(caller)
                 val textDecline = data.getString(CallkitConstants.EXTRA_CALLKIT_TEXT_DECLINE, "")
                 val declineAction: NotificationCompat.Action = NotificationCompat.Action.Builder(
-                    0,
+                    R.drawable.ic_decline,
                     if (TextUtils.isEmpty(textDecline)) context.getString(R.string.text_decline) else textDecline,
                     getDeclinePendingIntent(notificationId, data)
                 ).build()
                 notificationBuilder.addAction(declineAction)
                 val textAccept = data.getString(CallkitConstants.EXTRA_CALLKIT_TEXT_ACCEPT, "")
                 val acceptAction: NotificationCompat.Action = NotificationCompat.Action.Builder(
-                    0,
+                    R.drawable.ic_accept,
                     if (TextUtils.isEmpty(textDecline)) context.getString(R.string.text_accept) else textAccept,
                     getAcceptPendingIntent(notificationId, data)
                 ).build()
